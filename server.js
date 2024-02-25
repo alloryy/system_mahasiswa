059ef4bb5a26e8324b8db569ab4f15d4e9450ef1
Davit Khoirul Mustakhim <davitkhoirulm@gmail.com>
2024-02-25 07:14:18 -1200

    setting routes at server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const routes = require('./routes');
 const app = express()
 const port = 3200
 
+
+// parse aplication json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// calling routes
+
+var routes = require('./routes')
+routes(app)
 
  app.listen(port, () => {
     console.log(`Server started on port`);
