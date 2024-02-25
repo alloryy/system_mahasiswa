f5ff80e68b0203d9eb5e52ea2c711c1baca3237b
Davit Khoirul Mustakhim <116443187+alloryy@users.noreply.github.com>
2024-02-25 07:44:13 -1200

    Update controler.js

--- controler.js
+++ controler.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok('rest api is runing')
+    response.ok('rest api is runing',res)
 }
